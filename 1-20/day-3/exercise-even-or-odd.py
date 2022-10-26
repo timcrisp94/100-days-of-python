5f07bf483d71cf36ffd14f3a5f9410683c16df7c
Tim Crisp <timothycrisp@gmail.com>
2022-10-26 14:57:49 -0500

    create day 3 directory, solve even or odd exercise

---
+++ 1-20/day-3/exercise-even-or-odd.py
@@ -0,0 +1,6 @@
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print(f"{number} is even because {number} / 2 = {number/2}")
+else:  
+  print(f"{number} is odd because {number} / 2 = {number/2}")
