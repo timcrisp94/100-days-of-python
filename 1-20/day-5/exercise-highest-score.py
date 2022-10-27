b65375e3d9ad21e0240f5366f1c046d3f056938e
Tim Crisp <timothycrisp@gmail.com>
2022-10-27 13:52:02 -0500

    finish python password generator project

---
+++ 1-20/day-5/exercise-highest-score.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 
+
+# 78 65 89 86 55 91 64 89
+
+max = 0
+for score in student_scores:
+  if score > max:
+    max = score
+
+print(f"The highest score in the class is {max}")
--- 1-20/day-5/notes.py
+++ 1-20/day-5/notes.py
@@ -4,4 +4,35 @@ for fruit in fruits:
 
 # "Anyway, like I was sayin', shrimp is the fruit of the sea. You can barbecue it, boil it, broil it, bake it, saute it. There's uh, shrimp-kabobs, shrimp creole, shrimp gumbo. Pan fried, deep fried, stir-fried. There's pineapple shrimp, lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup, shrimp stew, shrimp salad, shrimp and potatoes, shrimp burger, shrimp sandwich. That- that's about it."
 
-it = ["barbeque"]
+it = ["barbeque", "boil", "broil", "bake", "saute"]
+shrimp_dash = ["-kababs", "creole", "gumbo"]
+fried = ["pan", "deep", "stir-"]
+fruit = ["pineapple", "lemon", "coconut", "pepper"]
+dish = ["soup", "stew", "salad", "and potatoes", "burger", "sandwich" ]
+
+
+# for number in range(1, 10):
+#   print(number) #prints 1-9
+
+# for number in range(1, 11):
+#   print(number) # 1-10
+
+# for number in range(1, 11, 2):
+#   print(number) #increments by 2
+
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
+
+even_total = 0
+for number in range(2, 101, 2):
+  even_total += number
+
+print(even_total)
+
+odd_total = 0
+for number in range(1, 100, 2):
+  odd_total += number
+
+print(odd_total)
