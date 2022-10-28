6835b0efe3486a10cce147769497f084c42454cb
Tim Crisp <timothycrisp@gmail.com>
2022-10-28 11:27:50 -0500

    create display variable, update display variable with correct guesses

--- 1-20/day-7/hangman.py
+++ 1-20/day-7/hangman.py
@@ -1,19 +1,35 @@
-#Step 1 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-chosen_word = word_list[random.randint(0, 2)]
+chosen_word = word_list[random.randint(0, (len(word_list) - 1))]
+
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+  display += "_"
+print(display)
 
 guess = input("Player, guess a letter ").lower()
 
-for letter in chosen_word:
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+for position in range(word_length):
+  letter = chosen_word[position]
   if letter == guess: 
-    print("True ")
-  else: 
-    print("False ")
+    display[position] = letter
+
+print(display)
+ 
 
 
 
