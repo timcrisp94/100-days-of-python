3b0c5b21981331c33c847dd4002b5b3d76f6122f
Tim Crisp <timothycrisp@gmail.com>
2022-10-31 13:25:16 -0500

    complete caesar cypher

--- 1-20/day-8/project-caesar-cypher.py
+++ 1-20/day-8/project-caesar-cypher.py
@@ -1,21 +1,40 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # combine encrypt and decrypt functions into single function called caesar
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
     shift_amount *= -1
-  for letter in start_text:
+  for char in start_text:
+    if not char in alphabet:
+      end_text += char
+    position = alphabet.index(char)
     new_position = position + shift_amount
-    position = alphabet.index(letter)
     end_text += alphabet[new_position]
   print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+print(logo)
+
+game_over = False
+while not game_over:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Want to play again? Type 'yes' if you want to play again. Otherwise type 'no' ")
+  if restart == "no":
+    game_over = True
+    print("bye bye")
+
+
+  
+  
+
 
 
 # # encrypt function 
