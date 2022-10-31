0d6d984fd6f72352092cac6b7d5aba3664859c73
Tim Crisp <timothycrisp@gmail.com>
2022-10-31 12:33:13 -0500

    create caesar function using components of encrypt and decrypt functions

--- 1-20/day-8/project-caesar-cypher.py
+++ 1-20/day-8/project-caesar-cypher.py
@@ -1,50 +1,55 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+# combine encrypt and decrypt functions into single function called caesar
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for letter in start_text:
+    new_position = position + shift_amount
+    position = alphabet.index(letter)
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}")
 
-# encrypt function 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ''
-  for char in plain_text:
-    # shift forward
-    for i, letter in enumerate(alphabet):
-      if char == letter:
-        if i + shift_amount > 26:
-          cipher_text += alphabet[i + shift_amount - 26]
-        else:
-          cipher_text += alphabet[i + shift_amount]
-  print(f"the encoded text is {cipher_text}")
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+# # encrypt function 
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ''
+#   for char in plain_text:
+#     # shift forward
+#     for i, letter in enumerate(alphabet):
+#       if char == letter:
+#         if i + shift_amount > 26:
+#           cipher_text += alphabet[i + shift_amount - 26]
+#         else:
+#           cipher_text += alphabet[i + shift_amount]
+#   print(f"the encoded text is {cipher_text}")
 
 # test encrypt function
 # encrypt(plain_text="zulu", shift_amount=5) 
 
 # decrypt function
-def decrypt(cipher_text, shift_amount):
-  plain_text = ''
-  for char in cipher_text:
-    # shift back
-    for i, letter in enumerate(alphabet):
-      if char == letter:
-        if i - shift_amount < 0:
-          plain_text += alphabet[26 + i - shift_amount]
-        else:
-          plain_text += alphabet[i - shift_amount]
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ''
+#   for char in cipher_text:
+#     # shift back
+#     for i, letter in enumerate(alphabet):
+#       if char == letter:
+#         if i - shift_amount < 0:
+#           plain_text += alphabet[26 + i - shift_amount]
+#         else:
+#           plain_text += alphabet[i - shift_amount]
 
-  print(f"the decoded text is {plain_text}")
+#   print(f"the decoded text is {plain_text}")
 
 # test decrypt
 # decrypt(cipher_text="mjqqt", shift_amount=5)
-
-# encode or decode
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
-
-
 
 
 
