3794aed07b1ddec6042aee7f1cfaeeec5c798441
Tim Crisp <timothycrisp@gmail.com>
2022-11-01 14:05:15 -0500

    move calculator logic into calculator function, recursively call calculator function in while calculating loop if user wants to start a new calculation

--- 1-20/day_10/project_calculator.py
+++ 1-20/day_10/project_calculator.py
@@ -1,9 +1,15 @@
 from art import logo
+from os import system, name
 
+# clear function
+def clear():
+	# for windows
+	if name == 'nt':
+		_ = system('cls')
+	# for mac and linux(here, os.name is 'posix')
+	else:
+		_ = system('clear')
 
-
-
-# calculator
 
 # add
 def add(n1, n2):
@@ -28,37 +34,39 @@ operations = {
   "*": multiply,
   "/": divide,
 }
-
-# num1 input
-num1 = int(input("what's the first number? "))
-calculating = True
-
-
-# loop through dictionary, print keys, and ask which operation
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-
-# num2 input
-num2 = int(input("what's the second number? "))
-
-# first answer method
-calculation = operations[operation_symbol]
-first_answer = calculation(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-# continue calculating
-while calculating:
-  continue_calculating = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit.: ")
-  if continue_calculating == 'y':
-    next_num = int(input("What's the next number? "))
-    operation_symbol = input("Pick another operation: ")
-    next_answer = calculation(first_answer, next_num)
-    print(f"{first_answer} {operation_symbol} {next_num} = {next_answer}")
-  else:
-    calculating = False
-    print('good-bye')
+def calculator():
+  print(logo)
+  # num1 input
+  num1 = int(input("what's the first number? "))
+  calculating = True
 
 
-# second answer
+  # loop through dictionary, print keys, and ask which operation
+  for symbol in operations:
+    print(symbol)
+  operation_symbol = input("Pick an operation from the line above: ")
+
+  # num2 input
+  num2 = int(input("what's the second number? "))
+
+  # first answer method
+  calculation = operations[operation_symbol]
+  first_answer = calculation(num1, num2)
+  print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+  # continue calculating
+  while calculating:
+    continue_calculating = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start a new calculation: ")
+    if continue_calculating == 'y':
+      operation_symbol = input("Pick another operation: ")
+      next_num = int(input("What's the next number? "))
+      next_answer = calculation(first_answer, next_num)
+      print(f"{first_answer} {operation_symbol} {next_num} = {next_answer}")
+    else:
+      calculating = False
+      clear()
+      calculator()
+
+
+calculator()
 
