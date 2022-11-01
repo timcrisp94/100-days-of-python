26923d528dc0062f864da7af541c3866d5a4053c
Tim Crisp <timothycrisp@gmail.com>
2022-11-01 14:17:41 -0500

    consolidate calculator function, combine second answer with next answer, set num1 = to answer for continued calculating

--- 1-20/day_10/project_calculator.py
+++ 1-20/day_10/project_calculator.py
@@ -10,7 +10,6 @@ def clear():
 	else:
 		_ = system('clear')
 
-
 # add
 def add(n1, n2):
   return n1 + n2
@@ -34,34 +33,28 @@ operations = {
   "*": multiply,
   "/": divide,
 }
+
 def calculator():
   print(logo)
   # num1 input
   num1 = int(input("what's the first number? "))
   calculating = True
 
-
-  # loop through dictionary, print keys, and ask which operation
-  for symbol in operations:
-    print(symbol)
-  operation_symbol = input("Pick an operation from the line above: ")
-
-  # num2 input
-  num2 = int(input("what's the second number? "))
-
-  # first answer method
-  calculation = operations[operation_symbol]
-  first_answer = calculation(num1, num2)
-  print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-  # continue calculating
   while calculating:
-    continue_calculating = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start a new calculation: ")
-    if continue_calculating == 'y':
-      operation_symbol = input("Pick another operation: ")
-      next_num = int(input("What's the next number? "))
-      next_answer = calculation(first_answer, next_num)
-      print(f"{first_answer} {operation_symbol} {next_num} = {next_answer}")
+    # loop through dictionary, print keys, and ask which operation
+    for symbol in operations:
+      print(symbol)
+    operation_symbol = input("Pick an operation from the line above: ")
+    # num2 input
+    num2 = float(input("what's the next number? "))
+
+    # first answer method
+    calculation = operations[operation_symbol]
+    answer = calculation(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    # continue ?
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
     else:
       calculating = False
       clear()
