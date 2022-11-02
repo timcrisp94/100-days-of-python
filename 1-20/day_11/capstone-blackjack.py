d0b2f3bb45e7b03c5a359c8bdda71cabc9563d35
Tim Crisp <timothycrisp@gmail.com>
2022-11-02 12:18:13 -0500

    move call to deal cards up to deal with scoping issue

--- 1-20/day_11/capstone-blackjack.py
+++ 1-20/day_11/capstone-blackjack.py
@@ -22,88 +22,65 @@ def clear():
 
 # environment variables
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_cards = []
-computer_cards = []
-
-player_score = 0
-computer_score = 0
-
-
-# def update_player_score():
-#   if player_cards == [10, 11]:
-#     print("blackjack")
-
-#   if player_score > 21:
-#     for card in player_cards:
-#       if card == 11:
-#         card = 1
-#         player_score -= 10
-#     return player_score
-
-#   player_score = sum(player_cards)
-#   return player_score
-
-  
 
 
 # game function
 def game():
 
+  player_cards = []
+  computer_cards = []
+
+  player_score = 0
+  computer_score = 0
   
   print(logo)
 
-  def deal_cards():
-    player_turn = True
-    computer_turn = False
-    
-    # player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
-    player_cards = [12, 12]
+  def deal_cards():  
+    player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
     player_score = sum(player_cards)
-    print(player_cards, player_score)
 
-    if player_cards == [10, 11]:
-      print("black jack")
-
-    if player_score > 21:
-      print("over_21")
-      for index, card in enumerate(player_cards):
-        if not card == 11:
-          print("game over")
-        
-        
-        
-
+    
     computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]      
+    computer_score = sum(computer_cards)
 
     print(f"Your cards: {player_cards}, current score: {player_score}")
     print(f"Computer's first card: {computer_cards[0]}")
-    
-    def draw_player_card():
-      player_cards.append(cards[random.choice(cards)])
-      player_score = sum(player_cards)
-      print(player_cards, player_score)
-    
-    while player_turn == True:
-      if input(f"Do you want to hit? Type 'y' or 'n' ") == 'y':
-        draw_player_card()
-      else:
-        player_turn = False
-        computer_turn = True
-        print(player_score)
-
-    def draw_computer_card():
-      computer_cards.append(cards[random.choice(cards)])
-      computer_score = sum(computer_cards)
-      print(computer_cards, computer_score)
-
-    while player_turn == False and computer_turn == True:
-      computer_turn = False
-      draw_computer_card()    
-
-    if player_turn == False and computer_turn == False:
-      print("game over")
 
   deal_cards()
+  player_turn = True
+  computer_turn = False
+
+  def draw_player_card():
+    player_cards.append(cards[random.choice(cards)])
+    player_score = sum(player_cards)
+    print(player_cards, player_score)
+  
+  while player_turn == True:
+    if input(f"Do you want to hit? Type 'y' or 'n' ") == 'y':
+      draw_player_card()
+    else:
+      player_turn = False
+      computer_turn = True
+      print(player_score)
+
+  def draw_computer_card():
+    computer_score = sum(computer_cards)
+    if computer_score < 17:
+      computer_cards.append(cards[random.choice(cards)])
+      computer_score = sum(computer_cards)
+    else:
+      print(computer_score)
+
+  while player_turn == False and computer_turn == True:
+    computer_turn = False
+    draw_computer_card()    
+
+  if player_turn == False and computer_turn == False: 
+    if player_score > computer_score:
+      print(player_score, computer_score)
+      print("player wins")
+      
+
 
 # ask user if they want to start the game
 if input(f"Do you want to play a game of blackjack? Type 'y' or 'n' ") == 'y':
