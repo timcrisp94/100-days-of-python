2819c951385ff62fb2f6102b7c9ccab931c66057
Tim Crisp <timothycrisp@gmail.com>
2022-11-02 13:24:28 -0500

    set up blackjack conditional, still working with over 21

--- 1-20/day_11/capstone-blackjack.py
+++ 1-20/day_11/capstone-blackjack.py
@@ -26,25 +26,42 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 # game function
 def game():
-
-  player_cards = []
-  computer_cards = []
-
-  player_score = 0
-  computer_score = 0
   
   print(logo)
 
   def deal_cards():  
-    player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
+    player_cards = [12, 11]
+    # player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
     player_score = sum(player_cards)
     # computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
     computer_cards = [3, 3]
     computer_score = sum(computer_cards)      
 
-    # blackjack conditions
+    # black jack conditions
+    if player_cards.sort() == [10, 11]:
+      return "player wins with a blackjack"
+    
+    if computer_cards.sort == [10, 11]:
+      return "computer wins with a blackjack"
+    
     # over_21 conditions
-
+    if player_score > 21:
+      if computer_score - 10 < 21:
+        for i, card in enumerate(player_cards):
+          if card == 11:
+            player_cards[i] = 1
+            player_score -= 10
+            break
+      else:
+        print("bust")
+    
+    if computer_score > 21:
+        for i, card in enumerate(player_cards):
+          if card == 11:
+            player_cards[i] = 1
+            player_score -= 10
+            break
+        
     print(f"Your cards: {player_cards}, current score: {player_score}")
     print(f"Computer's first card: {computer_cards[0]}")
     
@@ -65,29 +82,8 @@ def game():
 
       if computer_score > 17:
         computer_turn = False
-    
-    # def draw_computer_card():
-    #     computer_cards.append(cards[random.choice(cards)])
-    #     computer_score = sum(computer_cards)
-
-    # if player_turn == False and computer_turn == True:
-    #   if computer_score < 17:
-    #     draw_computer_card()
-    #   else:
-    #     computer_turn = False
-    #     print(computer_cards, computer_score)
-         
 
   deal_cards()
-
-
-
-  # if player_turn == False and computer_turn == False: 
-  #   if player_score > computer_score:
-  #     print(player_score, computer_score)
-  #     print("player wins")
-      
-
 
 # ask user if they want to start the game
 if input(f"Do you want to play a game of blackjack? Type 'y' or 'n' ") == 'y':
