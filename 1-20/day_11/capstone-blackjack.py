b7085c92566fb5fbab2d12af87904fb09a948afe
Tim Crisp <timothycrisp@gmail.com>
2022-11-02 11:37:13 -0500

    add to over_21 condition

--- 1-20/day_11/capstone-blackjack.py
+++ 1-20/day_11/capstone-blackjack.py
@@ -29,21 +29,52 @@ player_score = 0
 computer_score = 0
 
 
+# def update_player_score():
+#   if player_cards == [10, 11]:
+#     print("blackjack")
+
+#   if player_score > 21:
+#     for card in player_cards:
+#       if card == 11:
+#         card = 1
+#         player_score -= 10
+#     return player_score
+
+#   player_score = sum(player_cards)
+#   return player_score
+
+  
+
 
 # game function
 def game():
 
+  
   print(logo)
 
   def deal_cards():
     player_turn = True
     computer_turn = False
     
-    player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
+    # player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
+    player_cards = [12, 12]
     player_score = sum(player_cards)
+    print(player_cards, player_score)
 
-    computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
-    
+    if player_cards == [10, 11]:
+      print("black jack")
+
+    if player_score > 21:
+      print("over_21")
+      for index, card in enumerate(player_cards):
+        if not card == 11:
+          print("game over")
+        
+        
+        
+
+    computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]      
+
     print(f"Your cards: {player_cards}, current score: {player_score}")
     print(f"Computer's first card: {computer_cards[0]}")
     
@@ -67,46 +98,12 @@ def game():
 
     while player_turn == False and computer_turn == True:
       computer_turn = False
-      draw_computer_card()
-      
+      draw_computer_card()    
 
     if player_turn == False and computer_turn == False:
       print("game over")
 
   deal_cards()
-  
-  
-
-  # # function to check if either player has blackjack
-  # def blackjack(score):
-  #   if score == 21:
-  #     return True
-  #   else:
-  #     return False
-
-  # # function to switch ace if over 21
-  # def over_21(score, hand):
-  #   if score > 21:
-  #     for card in hand:
-  #       if card == 11:
-  #         card = 1
-  #         score -= 10
-  #   return score
-
-  # test over_21
-  # print(over_21(26, [11, 10, 5]))
-
-  # test hit
-
-  # while not blackjack(player_score) and not blackjack(computer_score):
-  #   if not over_21(player_score, player_cards):
-  #       print(hit(player_score, player_cards))
-        # print(f"Your cards: {player_cards}, current score: {player_score}")
-
-
-
-
-
 
 # ask user if they want to start the game
 if input(f"Do you want to play a game of blackjack? Type 'y' or 'n' ") == 'y':
