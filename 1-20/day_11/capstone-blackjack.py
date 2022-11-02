cb3b9decc2e74a4cbf8cda78c29f7dd6f4990c25
Tim Crisp <timothycrisp@gmail.com>
2022-11-02 15:34:43 -0500

    create deal card function to deal cards one at a time, add blackjack hand and over 21 conditionals to update scare function

--- 1-20/day_11/capstone-blackjack.py
+++ 1-20/day_11/capstone-blackjack.py
@@ -20,70 +20,66 @@ def clear():
 	else:
 		_ = system('clear')
 
-# environment variables
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# deal card function
+def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+# update score function
+def update_score(cards):
+  # check for blackjack hand
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  # if over 21, check for an ace
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
+
 
 
 # game function
 def game():
-  
+    
+
   print(logo)
 
-  def deal_cards():  
-    player_cards = [12, 11]
-    # player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
-    player_score = sum(player_cards)
-    # computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
-    computer_cards = [3, 3]
-    computer_score = sum(computer_cards)      
+  # def deal_cards():      
+  #   player_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
+  #   player_score = update_score(0, player_cards)
+    
+  #   computer_cards = [cards[random.choice(cards)], cards[random.choice(cards)]]
+  #   computer_score = update_score(0, computer_cards)
+    
+  #   if over_21(player_score, player_cards):
+  #     print("bust")
+  #   elif over_21(computer_score, computer_cards):
+  #     print("bust")
+  #   else:
+  #     print(f"Your cards: {player_cards}, current score: {player_score}")
+  #     print(f"Computer's first card: {computer_cards[0]}")
+    
+  #   computer_turn = False
+    
+  #   while input(f"Do you want to hit? Type 'y' or 'n' ") == 'y':
+  #     player_cards.append(cards[random.choice(cards)])
+  #     player_score = update_score(player_score, player_cards)
+  #     if not over_21(player_score, player_cards):
+  #       print(player_score, player_cards)
+  #   else:
+  #     print(player_cards, player_score)
+  #     computer_turn = True
 
-    # black jack conditions
-    if player_cards.sort() == [10, 11]:
-      return "player wins with a blackjack"
-    
-    if computer_cards.sort == [10, 11]:
-      return "computer wins with a blackjack"
-    
-    # over_21 conditions
-    if player_score > 21:
-      if computer_score - 10 < 21:
-        for i, card in enumerate(player_cards):
-          if card == 11:
-            player_cards[i] = 1
-            player_score -= 10
-            break
-      else:
-        print("bust")
-    
-    if computer_score > 21:
-        for i, card in enumerate(player_cards):
-          if card == 11:
-            player_cards[i] = 1
-            player_score -= 10
-            break
-        
-    print(f"Your cards: {player_cards}, current score: {player_score}")
-    print(f"Computer's first card: {computer_cards[0]}")
-    
-    computer_turn = False
-    
-    while input(f"Do you want to hit? Type 'y' or 'n' ") == 'y':
-      player_cards.append(cards[random.choice(cards)])
-      player_score = sum(player_cards)
-      print(player_cards, player_score)
-    else:
-      print(player_cards, player_score)
-      computer_turn = True
+  #   while computer_turn == True and computer_score < 17:
+  #     computer_cards.append(cards[random.choice(cards)])
+  #     update_score(computer_score, computer_cards)
+  #     print(computer_cards, computer_score)
 
-    while computer_turn == True and computer_score < 17:
-      computer_cards.append(cards[random.choice(cards)])
-      computer_score = sum(computer_cards)
-      print(computer_cards, computer_score)
+  #     if computer_score > 17:
+  #       computer_turn = False
 
-      if computer_score > 17:
-        computer_turn = False
-
-  deal_cards()
+  # deal_cards()
 
 # ask user if they want to start the game
 if input(f"Do you want to play a game of blackjack? Type 'y' or 'n' ") == 'y':
