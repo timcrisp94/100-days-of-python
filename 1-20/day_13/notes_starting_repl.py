6680cdd03953ff62810b8f44aeb7d88bc6e05911
Tim Crisp <timothycrisp@gmail.com>
2022-11-04 14:35:54 -0500

    uniform directory names

---
+++ 1-20/day_13/notes_starting_repl.py
@@ -0,0 +1,70 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+"""
+we have a function, when called function
+  runs for loop for i in range 1 to 20
+    if i == 20: print you got it
+
+the problem, nothing prints
+  range is 1 to 20, but the range function does not include the end value
+
+to fix the problem, set range = 1 to 21
+"""
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+"""
+list index out of range error when index == 6
+  change randint parameters to (0, 5)
+"""  
+
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
