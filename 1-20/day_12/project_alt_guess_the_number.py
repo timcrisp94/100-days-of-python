40fb7063412f7ba04c4933eccebfe4bb0c2915b2
Tim Crisp <timothycrisp@gmail.com>
2022-11-04 14:06:05 -0500

    finish alt solution to guess the number, create check_answer, set_difficulty, and game functions

--- 1-20/day_12/project_alt_guess_the_number.py
+++ 1-20/day_12/project_alt_guess_the_number.py
@@ -15,3 +15,50 @@ EASY_LEVEL = 10
 HARD_LEVEL = 5
 
 # check user answer against actual answer
+def check_answer(guess, answer, turns):
+  # compare guess to answer
+    # return number of turns remaining
+  if guess > answer:
+    print("Too hight")
+    return turns - 1
+  elif guess < answer:
+    print("Too low")
+    return turns - 1
+  else:
+    print(f"You got it! The answer is {answer}")
+
+def set_difficulty():
+  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  if difficulty[0] == 'e':
+    return EASY_LEVEL
+  else:
+    return HARD_LEVEL
+
+def game():
+  
+  print(logo)
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+
+  number = random.randint(1, 101)
+  # test
+  # print(f"the number is {number}")
+
+  # set number of turns = set_difficulty()
+  turns = set_difficulty()
+  # guess until out of turns
+  guess = 0
+  while not guess == number:
+    print(f"You have {turns} attempts remaining to guess the number")
+    
+    # let user guess a number
+    guess = int(input("Make a guess: "))
+
+    turns = check_answer(guess, number, turns)
+    if turns == 0:
+      print("You're out of guesses, you lose")
+      return
+    elif guess != number:
+      print("Guess again.")
+
+game()
