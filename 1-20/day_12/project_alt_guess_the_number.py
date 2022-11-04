15592930c289485fe1fc8886e0e348c01cf750cd
Tim Crisp <timothycrisp@gmail.com>
2022-11-04 08:54:21 -0500

    create alt solution doc

---
+++ 1-20/day_12/project_alt_guess_the_number.py
@@ -0,0 +1,17 @@
+"""
+initially completed the project by writing user stories and creating variables and conditions
+looking at the finished repl, i decided to go back and build using functions
+we're going to create functions to
+-check user answer against actual answer
+-set difficulty level to a global variable
+-game()
+"""
+
+from art import logo
+import random
+
+# global difficulty variable 
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
+# check user answer against actual answer
