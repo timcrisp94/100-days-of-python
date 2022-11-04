336c3f7d9798e310722a6a460f78dc7d9e0dba29
Tim Crisp <timothycrisp@gmail.com>
2022-11-04 15:05:38 -0500

    complete day 13

---
+++ 1-20/day_13/exercise_debugging.py
@@ -0,0 +1,20 @@
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else: 
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
