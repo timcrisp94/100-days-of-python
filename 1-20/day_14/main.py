155df567f018681d77044140d1422647240ac309
Tim Crisp <timothycrisp@gmail.com>
2022-11-07 07:25:36 -0600

    refactor random number generator to create function get_data for one person at a time; create format data function

--- 1-20/day_14/main.py
+++ 1-20/day_14/main.py
@@ -3,23 +3,23 @@ from game_data import data
 
 # generate random numbers to select data
 from random import randint
-person_one = data[randint(0, len(data))]
-person_two = data[randint(0, len(data))]
-# print(person_one, person_two)
+def get_data():
+  return data[randint(0, len(data))]
+
+# get_data()
 
 
-# TODO create compare function to compare A and B
-def compare(person_one, person_two):
-  person_one_follower_count = person_one["follower_count"]
-  person_two_follower_count = person_two["follower_count"]
-  # print(person_one_follower_count, person_two_follower_count)
-  if person_one_follower_count > person_two_follower_count:
-    return "A"
-  else:
-    return "B"
+# format data to compare followers
+def format_data(person):
+  name = person[name]
+  description = person[description]
+  country = person[country]
+  return f"{name} a {description} from {country}"
+  
 
-# compare(person_one=person_one, person_two=person_two)
-# TODO create player guess function
+# check answer function
+guess = input("Who has more followers? A or B? ")
+
 # TODO create compare function to compare A or B to player guess
 # TODO create game_over function
 
