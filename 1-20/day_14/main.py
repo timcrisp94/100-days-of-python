24bddc5659c2a4e3a88025a27482dd30372c33c0
Tim Crisp <timothycrisp@gmail.com>
2022-11-07 06:53:56 -0600

    create todo list, import randint, create person_one and person_two variables

--- 1-20/day_14/main.py
+++ 1-20/day_14/main.py
@@ -1,6 +1,21 @@
 from art import logo, vs
 from game_data import data
 
-print(logo)
-print(vs)
-print(data[0])
+# generate random numbers to select data
+from random import randint
+person_one = randint(0, len(data))
+person_two = randint(0, len(data))
+# print(person_one, person_two)
+
+
+# TODO create compare function to compare A and B
+
+# TODO create player guess function
+# TODO create compare function to compare A or B to player guess
+# TODO create game_over function
+
+
+
+# print(logo)
+# print(vs)
+# print(data[0])
