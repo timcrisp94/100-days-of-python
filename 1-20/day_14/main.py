c30f0b08ea796ff90af53c8bef513b7412f678da
Tim Crisp <timothycrisp@gmail.com>
2022-11-07 07:39:41 -0600

    create playing conditions using two while loops

--- 1-20/day_14/main.py
+++ 1-20/day_14/main.py
@@ -1,30 +1,56 @@
 from art import logo, vs
 from game_data import data
+import random
+import os
+
+# clear function
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 
 # generate random numbers to select data
-from random import randint
 def get_data():
-  return data[randint(0, len(data))]
-
-# get_data()
-
+  return random.choice(data)
 
 # format data to compare followers
 def format_data(person):
-  name = person[name]
-  description = person[description]
-  country = person[country]
+  name = person["name"]
+  description = person["description"]
+  country = person["country"]
   return f"{name} a {description} from {country}"
   
-
 # check answer function
-guess = input("Who has more followers? A or B? ")
+def check_answer(guess, person_a_followers, person_b_followers):
+  if person_a_followers > person_b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
 
-# TODO create compare function to compare A or B to player guess
-# TODO create game_over function
+
+def game():
+  print(logo)
+  score = 0
+  game_over = False
+
+  # generate persons a and b
+  person_a = get_data()
+  person_b = get_data()
+  
+  while not game_over:
+    person_a = person_b
+    person_b = get_data()
+
+    while person_a == person_b:
+      person_b = get_data()
+
+      print(f"Compare A: {format_data(person_a)}")
+      print(vs)
+      print(f"Compare B: {format_data(person_b)}")
 
 
 
 # print(logo)
 # print(vs)
 # print(data[0])
+
+game()
