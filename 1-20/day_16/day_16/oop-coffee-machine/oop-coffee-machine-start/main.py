f4e2ff9e1af3b57bbb9bee632350335a45c4b611
Tim Crisp <timothycrisp@gmail.com>
2022-11-09 11:49:01 -0600

    begin oop refactor of coffee-maker program

--- 1-20/day_16/day_16/oop-coffee-machine/oop-coffee-machine-start/main.py
+++ 1-20/day_16/day_16/oop-coffee-machine/oop-coffee-machine-start/main.py
@@ -1,3 +1,37 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+"""
+# 1. Print Report
+# 2. Check Resources Sufficient
+# 3. Process Coins
+# 4. Check Transaction Successful
+# 5. Make Drink
+ """
+
+#
+coffee_machine = CoffeeMaker()
+cash_register = MoneyMachine()
+drink_menu = Menu()
+
+spro = drink_menu.find_drink("espresso")
+cap = drink_menu.find_drink("cappuccino")
+tay = drink_menu.find_drink("latte")
+
+
+def take_order(drink_order):
+    if drink_order == "print report":
+        coffee_machine.report()
+    elif drink_order == "off":
+        print("Goodbye")
+    else:
+        if coffee_machine.is_resource_sufficient(drink_order):
+            return True
+        else:
+            return False
+
+
+order = input("What would you like to drink? latte/espresso/cappuccino?: ")
+
+take_order(order)
