afbbb523a10f422e9316c8a55d2dedb90c5e5963
Tim Crisp <timothycrisp@gmail.com>
2022-11-09 15:05:00 -0600

    complete coffee_maker in oop

--- 1-20/day_16/day_16/oop-coffee-machine/oop-coffee-machine-start/main.py
+++ 1-20/day_16/day_16/oop-coffee-machine/oop-coffee-machine-start/main.py
@@ -13,10 +13,19 @@ from money_machine import MoneyMachine
 #imported functions
 coffee_machine = CoffeeMaker()
 cash_register = MoneyMachine()
-drink_menu = Menu()
+menu = Menu()
 
 is_on = True
 
 while is_on:
-    options = drink_menu.get_items()
-    order = input("What would you like to drink? espresso/cappuccino/latte")
+    options = menu.get_items()
+    order = input("What would you like to drink? espresso/cappuccino/latte?: ")
+    if order == "print report":
+        coffee_machine.report()
+        cash_register.report()
+    elif order == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(order)
+        if coffee_machine.is_resource_sufficient(drink) and cash_register.make_payment(drink.cost):
+            coffee_machine.make_coffee(drink)
