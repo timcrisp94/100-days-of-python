1a8aa07e700a771fe2977db9d725970551dc2382
Tim Crisp <timothycrisp@gmail.com>
2022-11-10 14:06:32 -0600

    create day 18 directory, import turtle, add properties to Turtle object

---
+++ 1-20/day_18/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("square")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
