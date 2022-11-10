765159261b3d15e0fdcbcd760b80d4959910cf96
Tim Crisp <timothycrisp@gmail.com>
2022-11-10 14:28:04 -0600

    draw square with turtle

--- 1-20/day_18/main.py
+++ 1-20/day_18/main.py
@@ -3,13 +3,19 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("square")
 timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
+
+# draw a square
+def draw_square(turtle):
+  turtle.forward(100)
+
+  for i in range(0, 3):
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
+
+  return turtle
 
 
-
-
-
-
+# draw_square(timmy_the_turtle)
 
 
 
