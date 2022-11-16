09e0385312b6aea4cb6ae7e222364ab29c700534
Tim Crisp <timothycrisp@gmail.com>
2022-11-16 10:44:27 -0600

    add main dot py document, review for loop and range function

---
+++ 1-20/day_5/main.py
@@ -0,0 +1,34 @@
+# For Loop
+# fruits = ["Apple", "Peach", "Pear"]
+
+# access single item in a list
+# for fruit in fruits:
+#     print(fruit)
+
+# range function
+# range(a, b)
+# not including b
+# for number in range(1, 10):
+#     # prints 1-9
+#     print(number)
+
+# for number in range(1, 11):
+#     # prints 1-10
+#     print(number)
+
+# for number in range(1, 21, 2):
+#     # prints every odd
+#     print(number)
+
+# total = 0
+# for n in range(1, 101):
+#     total += n
+
+# print(total)
+
+# sum = 0
+# for n in range(2, 101, 2):
+#     sum += n
+
+# print(sum)
+
