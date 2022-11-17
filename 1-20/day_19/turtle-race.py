5665bf834707b637b0cee2a00050c46cda52e575
Tim Crisp <timothycrisp@gmail.com>
2022-11-17 12:23:29 -0600

    set turtles in a line along the x coordinate -230

--- 1-20/day_19/turtle-race.py
+++ 1-20/day_19/turtle-race.py
@@ -1,7 +1,23 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+# # pass in keyword arguments for readability :)
+screen.setup(width=500, height=400) 
+# user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+# set y-positions list for turtle_index
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+# create turtles
+# it's ok to have individual objects sharing the same name
+# colors differentiate them
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
 
 
 screen.exitonclick()
