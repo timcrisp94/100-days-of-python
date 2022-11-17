f3907c4a3d23f18967af7dfc6cef970dde5b6fdc
Tim Crisp <timothycrisp@gmail.com>
2022-11-17 10:25:35 -0600

    complete etch a sketch

---
+++ 1-20/day_19/main.py
@@ -0,0 +1,20 @@
+"""
+Event Listeners
+"""
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.exitonclick()
+
+"""
+Higher Order functions work with other functions as parameters 
+
+"""
