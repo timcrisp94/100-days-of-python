06926851ca2994ef0302dab131c3a60a62cee50b
Tim Crisp <timothycrisp@gmail.com>
2022-11-17 12:39:55 -0600

    complete turtle race, day 19

--- 1-20/day_19/turtle-race.py
+++ 1-20/day_19/turtle-race.py
@@ -1,23 +1,43 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 # # pass in keyword arguments for readability :)
-screen.setup(width=500, height=400) 
-# user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-
-# set y-positions list for turtle_index
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-# create turtles
-# it's ok to have individual objects sharing the same name
-# colors differentiate them
+#Create 6 turtles
+"""
+-we're creating multiple instances within the same class
+-each object has independent state
+"""
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
