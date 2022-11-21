61fec29fa990e6798a65f65ceb2a39fff16fef1c
Tim Crisp <timothycrisp@gmail.com>
2022-11-21 11:40:21 -0600

    finalize creating snake code, add pseudo code and a segments array, append each new segment to array

--- 1-20/day_20/main.py
+++ 1-20/day_20/main.py
@@ -6,15 +6,19 @@ screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
-# Snake
-starting_positions = [(0, 0), (-40, 0)]
+# Build snake by individual segments, starting with a snake at position 0 and moving left
+# for each position, add a white block segment to the snake (segments[])
+starting_positions = [(0, 0), (-20, 0) (-40, 0)]
+
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
     new_segment.goto(position)
+    segments.append(new_segment)
 
-
+# move snake forward
 
 
 
