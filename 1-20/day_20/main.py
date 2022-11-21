836ee48aa8d9f5d7736a371cbda626040b0776b4
Tim Crisp <timothycrisp@gmail.com>
2022-11-21 11:29:04 -0600

    create initial snake body

--- 1-20/day_20/main.py
+++ 1-20/day_20/main.py
@@ -6,6 +6,17 @@ screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
+# Snake
+segment_one = Turtle("square")
+segment_one.color("white")
+
+segment_two = Turtle("square")
+segment_two.color("white")
+segment_two.setposition(-10.00, 0.00)
+
+segment_three = Turtle("square")
+segment_three.color("white")
+segment_three.setposition(10.00, 0.00)
 
 
 
@@ -15,7 +26,6 @@ screen.title("Snake")
 
 
 
- 
 
 
 screen.exitonclick()
