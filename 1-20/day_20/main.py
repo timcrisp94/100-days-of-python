55b5229066d8269dee5e133d7a15463515a400ca
Tim Crisp <timothycrisp@gmail.com>
2022-11-21 12:52:13 -0600

    create snake class, import snake into main dot py, remove snake code from main

--- 1-20/day_20/main.py
+++ 1-20/day_20/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
+
 
 # Screen
 screen = Screen()
@@ -9,35 +11,16 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("Snake")
 
-# Build snake by individual segments, starting with a snake at position 0 and moving left
-# for each position, add a white block segment to the snake (segments[])
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+# create a new snake
+snake = Snake()
 
 # move snake forward
 game_is_on = True
-while game_is_on:
-    # delay to smooth animation see: tracer   
-    time.sleep(.1)
-
-    # * loop through range of segments in reverse
-    # * range(start, stop, step) *
-    for seg_num in range(len(segments) - 1, 0, -1):
-        # second to last segment x-coor
-        new_x = segments[seg_num - 1].xcor()
-        # second to last segment y-coor        
-        new_y = segments[seg_num - 1].ycor()        
-        # last segment go to second to last segment
-        segments[seg_num].goto(new_x, new_y)
+while game_is_on:    
     screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 
 
---
+++ 1-20/day_20/snake.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+class Snake:
+    
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+        # second to last segment x-coor  
+            new_x = self.segments[seg_num - 1].xcor()
+        # second to last segment y-coor  
+            new_y = self.segments[seg_num - 1].ycor()        
+        # last segment go to second to last segment
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
