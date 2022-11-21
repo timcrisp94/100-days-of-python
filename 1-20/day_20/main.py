b71fc0f922490459d18196d6629a33c2b8c59a9f
Tim Crisp <timothycrisp@gmail.com>
2022-11-21 11:17:03 -0600

    import turtle and screen, set-up screen dimensions, title and color

---
+++ 1-20/day_20/main.py
@@ -0,0 +1,21 @@
+from turtle import Screen, Turtle
+
+# Screen
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("Snake")
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+screen.exitonclick()
